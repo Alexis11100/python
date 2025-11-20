d68cc1f22b97489fb8ccff3b7907346913e267f3
coole <alexisbentley564@gmail.com>
2025-11-19 22:45:42 -0600

    Added file

--- test_television.py
+++ test_television.py
@@ -11,3 +11,11 @@ def test_power():
     assert str(tv) == "Power = True, Channel = 0, Volume = 0"
     tv.power()
     assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+def test_mute():
+    tv = Television()
+    tv.power()
+    tv.volume_up()
+    tv.mute()
+    assert str(tv) == "Power = True, Channel = 0, Volume = 0"  # muted shows volume 0
+    tv.mute()
+    assert str(tv) == "Power = True, Channel = 0, Volume = 1"  # unmuted restores volume
