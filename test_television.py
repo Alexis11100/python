1484c0ab6b7a85ebf66b47d34f99b23273ded355
coole <alexisbentley564@gmail.com>
2025-11-19 22:43:34 -0600

    Added file

--- test_television.py
+++ test_television.py
@@ -5,3 +5,9 @@ def test_init():
     tv = Television()
     # Use __str__ since variables are private
     assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+def test_power():
+    tv = Television()
+    tv.power()
+    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+    tv.power()
+    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
